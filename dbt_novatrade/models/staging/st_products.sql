3428f563921c4a637ebf186b13d6aabea2942abd
Chibueze <Chibuezeanalyst@gmail.com>
2026-05-01 06:14:36 -0500

    formated sql query

--- dbt_novatrade/models/staging/st_products.sql
+++ dbt_novatrade/models/staging/st_products.sql
@@ -1,9 +1,18 @@
-select
-    "ProductID",
-    "ProductName",
-    "Category",
-    "SubCategory",
-    "Brand",
-    "CostPrice",
-    "Tier"
-from {{ source('raw', 'ntg_products') }}
+with source as (
+    select *  from {{ source('raw', 'ntg_products')}}
+),
+
+cleaned as (
+    select
+        "ProductID"::varchar as productid,
+        "ProductName"::varchar as productname,
+        "Category"::varchar as category,
+        "SubCategory"::varchar as subcategory,
+        "Brand"::varchar as brand,
+        "CostPrice"::float as unitprice,
+        "Tier"::varchar as tier
+    from source 
+    where "ProductID" is not null
+)
+
+select * from cleaned
