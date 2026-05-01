d9e493164e6781cbff8da0191386937968d618fb
Chibueze <Chibuezeanalyst@gmail.com>
2026-05-01 06:24:50 -0500

    format sql

--- dbt_novatrade/models/staging/st_sales.sql
+++ dbt_novatrade/models/staging/st_sales.sql
@@ -1,13 +1,22 @@
-select
-    "TransactionID",
-    "OrderDate",
-    "CustomerID",
-    "ProductID",
-    "StoreID",
-    "Quantity",
-    "UnitPrice",
-    "DiscountPct",
-    "ReturnFlag",
-    "ReturnDate",
-    "ShipCost"
-from {{ source('raw', 'ntg_sales') }}
+with source as (
+    select * from {{ source('raw', 'ntg_sales') }}
+),
+
+cleaned as (
+    select
+        "TransactionID"::varchar as trxid,
+        "OrderDate"::date as orderdate,
+        "CustomerID"::varchar as customerid,
+        "ProductID"::varchar as productid,
+        "StoreID"::varchar as storeid,
+        "Quantity"::int as quantity,
+        round("UnitPrice"::numeric, 2) as unitprice,
+        round(("DiscountPct"::numeric / 100), 4) as discountpct,
+        "ReturnFlag"::boolean as returnflag,
+        "ReturnDate"::date as returndate,
+        round("ShipCost"::numeric, 2) as shipcost
+
+    from {{ source('raw', 'ntg_sales') }}
+)
+
+select * from cleaned
