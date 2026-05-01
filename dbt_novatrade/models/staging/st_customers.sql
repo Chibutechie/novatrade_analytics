c572f0568cee07ef5d7ea34f712bb40878ac7724
Chibueze <Chibuezeanalyst@gmail.com>
2026-05-01 05:37:59 -0500

    formated sql query

--- dbt_novatrade/models/staging/st_customers.sql
+++ dbt_novatrade/models/staging/st_customers.sql
@@ -1,10 +1,19 @@
-select 
-        "CustomerID",
-        "Segment",
-        "LoyaltyTier",
-        "Country",
-        "Region",
-        "JoinDate",
-        "Channel",
-        concat("FirstName", ' ', "LastName") as FullName
-from {{ source('raw', 'ntg_customers') }}
+with source as (
+    select * from {{ source('raw', 'ntg_customers') }}
+),
+
+cleaned as (
+    select 
+        "CustomerID"::varchar   as customerid,
+        "Segment"::varchar      as segment,    
+        "LoyaltyTier"::varchar  as loyaltytier,
+        "Country"::varchar      as country,
+        "Region"::varchar       as region,
+        "JoinDate"::date        as joindate,
+        "Channel"::varchar      as channel,
+	concat("FirstName", ' ', "LastName") as FullName
+    from source
+    where "CustomerID" is not null
+)
+
+select * from cleaned
